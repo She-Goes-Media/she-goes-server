03303c231c0a3847062c07aacca7b3fee2bff66b
Alicia.Lycan@nike.com <Alicia.Lycan@nike.com>
2018-10-20 00:41:40 -0700

    add express, bodyparser, cors, and set up boilerplate

--- server.js
+++ server.js
@@ -1,7 +1,41 @@
-// var express = require("express");
-var express = require("express");
+// const express = require("express");
+const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const app = express()
+const app = express();
+
+app.use(bodyParser.json());
+
+var corsOptions = {
+  origin: 'http://example.com',
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
+
+app.route('/api/shegoes').get((req, res) => {
+  res.send({
+    shegoes: [{firstName: 'Alicia'}, {lastName: 'Lycan'}]
+  });
+});
+
+app.route('/api/shegoes/:firstName').get((req, res) => {
+  const requestedName = req.params['firstName'];
+  res.send({ firstName: requestedName});
+});
+
+app.route('/api/shegoes').post((req, res) => {
+  res.send(201, req.body);
+});
+
+app.route('/api/shegoes').put((req, res) => {
+  res.send(200, req.body);
+});
+
+app.route('/api/shegoes/:firstName').delete((req, res) => {
+  res.sendStatus(204);
+});
 
 app.listen(3000, () => {
   console.log('Server started');
